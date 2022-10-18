2d9b930f56ffc50e2b24ed63959df960be7ad63f
Your Name <you@example.com>
2022-10-18 17:33:14 -0300

    Changing the location of the IStrategyWithKey interface.

--- StrategyHandler.DependencyInjection/IStrategyWithKey.cs
+++ StrategyHandler.DependencyInjection/IStrategyWithKey.cs
@@ -6,7 +6,7 @@
     /// <typeparam name="TRequest">The request to process.</typeparam>
     /// <typeparam name="TKey">The key to execute the method of class.</typeparam>
     /// <typeparam name="TResponse">The return.</typeparam>
-    public interface IStrategyWithKey<in TRequest, TKey, TResponse> 
+    public interface IStrategyWithKey<in TRequest, TKey, TResponse>
         : IStrategy<TRequest, TResponse>
         where TRequest : IRequest<TResponse>
     {
